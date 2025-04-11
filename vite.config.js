2d4eb77854bae2755e1283a7d1cb43c4ace118f9
Mohamed Moshihab <moshihab.dev@gmail.com>
2025-04-11 23:22:56 +0300

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/LoginPage/',  // <--- اسم المستودع الخاص بك
-  plugins: [react()],
-})
